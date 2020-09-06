708eb3cc522e3d538a0e94305797163f08576b9c
Lorik759 <vitors.alves@hotmail.com>
2020-09-05 22:06:12 -0300

    Criando e configurando modulos

---
+++ investigador-web/src/main/java/org/vtec/investigador/web/InvestigadorCoreApplication.java
@@ -0,0 +1,13 @@
+package org.vtec.investigador.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestigadorCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestigadorCoreApplication.class, args);
+	}
+
+}
---
+++ investigador-web/src/test/java/org/vtec/investigador/web/InvestigadorCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vtec.investigador.web;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestigadorCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
